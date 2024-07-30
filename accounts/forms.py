856058af6f96a9c31bf7fb682fb03f8b3da25a87
Prince Mfinanga <noahprince2@icloud.com>
2024-07-30 09:18:10 +0300

    new changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,12 +8,23 @@ class SaleForm(forms.ModelForm):
     class Meta:
         model = Sale
         fields = '__all__'
+        labels = {
+            'product': 'Jina la bidhaa',
+            'quantity_sold': 'Idadi iliyouzwa',
+            'selling_price': 'Bei ya kuuza',
+        }
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
+        labels = {
+            'name': 'Jina la bidhaa',
+            'description': 'Maelezo',
+            'cost_price': 'Bei ya kununua',
+            'selling_price': 'Bei ya kuuza',
+            'quantity': 'Idadi ilionunuliwa',
+        }
 
 class ExistingProductForm(forms.Form):
     product = forms.ModelChoiceField(queryset=Product.objects.all(), label='Chagua bidhaa')
